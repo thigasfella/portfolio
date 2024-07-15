909984fde325be4dc91edafe8b1e0698bcae1c40
Thiago Lopes de Souza <thiagolpssouza@gmail.com>
2024-07-15 00:09:21 -0300

    Adicionando arquivos iniciais do projeto do meu portfolio

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
+      <link
+            rel="stylesheet"
+            href="https://unpkg.com/aos@next/dist/aos.css"
+          />
+          <link
+            href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css"
+            rel="stylesheet"
+          />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        />
       <Head />
       <body>
         <Main />
