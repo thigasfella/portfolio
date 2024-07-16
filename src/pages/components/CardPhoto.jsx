c265c7e66da39056e7f5749214e7466e75691bc2
Thiago Lopes de Souza <thiagolpssouza@gmail.com>
2024-07-16 17:18:37 -0300

    Atualizações no projeto, pagina home

--- src/pages/components/CardPhoto.jsx
+++ src/pages/components/CardPhoto.jsx
@@ -1,20 +1,22 @@
-export default function CardPhoto(){
-    return(
-        <div className="container-photo" id="#home" data-aos="fade-down" data-aos-duration="1000">
-        <div className="card" style={{ width: '13rem' }}>
-          <img src="/img/my-photo.jpg" className="card-img-top" alt="Minha foto" width={300} height={320} />
-          <div className="card-body">
-            <p className="card-text">
-              <a href="http://github.com/thigasfella" target="_blank" rel="noopener noreferrer">
-                <i className="bi bi-github "></i>
-              </a>
-              <a href="https://www.linkedin.com/in/thiago-lopes-de-souza-a34930301/" target="_blank" rel="noopener noreferrer">
-                <i className="bi bi-linkedin"></i>
-              </a>
-            </p>
-            </div>
-          </div>
+export default function CardPhoto() {
+  return (
+    <div className="container-photo" id="home" data-aos="fade-down" data-aos-duration="1000">
+      <div className="card" style={{ width: '13rem' }}>
+        <img src="/img/my-photo.jpg" className="card-img-top" alt="Minha foto" width={300} height={320} />
+        <div className="card-body">
+          <p className="card-text">
+            <a href="https://wa.me/5562986251491?text=Ol%C3%A1!%20Vim%20do%20seu%20portfolio." target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-whatsapp small-icon"></i>
+            </a>
+            <a href="http://github.com/thigasfella" target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-github small-icon"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/thiago-lopes-de-souza-a34930301/" target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-linkedin small-icon"></i>
+            </a>
+          </p>
         </div>
-
-    )
+      </div>
+    </div>
+  );
 }
--- src/pages/components/TextVertical.jsx
+++ src/pages/components/TextVertical.jsx
@@ -1,8 +1,9 @@
+import TextApresentation from "./TextApresentation"
+
 const TextVertical = () => {
     return(
     <div className="vertical-text-container">
-        <p className="vertical-text-secondary" data-aos="fade-left" data-aos-duration="1000"><span className="destaque-text">JAVASCRIPT</span></p>
-        <p className="vertical-text" data-aos="fade-left" data-aos-duration="1000">DESENVOLVEDOR FULL STACK</p>
+        <TextApresentation/>
     </div>
     )
 }
