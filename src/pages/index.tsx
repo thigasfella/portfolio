0e7fad55dfdd6b6181299393965fb8287456a0ff
Thiago Lopes de Souza <thiagolpssouza@gmail.com>
2024-07-16 21:03:27 -0300

    Testando atualizações

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,8 +29,6 @@ export default function Home() {
               <CardPhoto />
 
               <TextApresentation />
-
-              <ImageComponent/>
  
               <TextVertical/>
         </section>
