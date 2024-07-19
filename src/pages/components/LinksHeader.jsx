f4468cfcbff2df97ad8ccb36c550e8413bcff90d
Thiago Lopes de Souza <thiagolpssouza@gmail.com>
2024-07-19 13:35:46 -0300

    tweste

--- src/pages/components/LinksHeader.jsx
+++ src/pages/components/LinksHeader.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import { useEffect } from 'react';
 
-export default function LinksHeader(){
-    return(
+export default function LinksHeader() {
+    useEffect(() => {
+        const links = document.querySelectorAll('.nav-link');
+        
+        links.forEach(link => {
+            link.addEventListener('click', (event) => {
+                event.preventDefault();
+                const targetId = event.target.getAttribute('data-target');
+                const targetElement = document.getElementById(targetId);
+
+                if (targetElement) {
+                    const elementTop = targetElement.getBoundingClientRect().top + window.pageYOffset;
+                    const elementHeight = targetElement.offsetHeight;
+                    const windowHeight = window.innerHeight;
+                    const scrollTo = elementTop - ((windowHeight - elementHeight) / 2);
+
+                    window.scrollTo({
+                        top: scrollTo,
+                        behavior: 'smooth'
+                    });
+                }
+            });
+        });
+
+        return () => {
+            links.forEach(link => {
+                link.removeEventListener('click', () => {});
+            });
+        };
+    }, []);
+
+    return (
         <header>
-        <a href="/">HOME</a>
-        <a href="#aboutMe">SOBRE MIM</a>
-        <a href="#projects">PROJETOS</a>
-        <a href="#contact">CONTATO</a>
+            <a href="#home" className="nav-link" data-target="home">HOME</a>
+            <a href="#aboutMe" className="nav-link" data-target="aboutMe">SOBRE MIM</a>
+            <a href="#projects" className="nav-link" data-target="projects">PROJETOS</a>
+            <a href="#contact" className="nav-link" data-target="contact">CONTATO</a>
         </header>
-    )
+    );
 }
--- src/pages/components/Projects.jsx
+++ src/pages/components/Projects.jsx
@@ -1,26 +1,37 @@
 import { useEffect, useState } from "react";
+import Image from 'next/image';
 
 const languageLogos = {
     "JavaScript": "https://raw.githubusercontent.com/github/explore/main/topics/javascript/javascript.png",
     "HTML": "https://raw.githubusercontent.com/github/explore/main/topics/html/html.png",
     "CSS": "https://raw.githubusercontent.com/github/explore/main/topics/css/css.png",
     "REACT": "https://raw.githubusercontent.com/github/explore/main/topics/react/react.png",
+    "NEXT.JS": "https://nextjs.org/static/favicon/favicon-32x32.png",
     "TypeScript": "https://raw.githubusercontent.com/github/explore/main/topics/typescript/typescript.png",
-    // Adicione outras linguagens e URLs de logos conforme necessário
+    "SASS": "https://raw.githubusercontent.com/github/explore/main/topics/sass/sass.png",
+    "Node.js": "https://raw.githubusercontent.com/github/explore/main/topics/nodejs/nodejs.png",
+    "MongoDB": "https://raw.githubusercontent.com/github/explore/main/topics/mongodb/mongodb.png",
+    "SQL": "https://raw.githubusercontent.com/github/explore/main/topics/sql/sql.png",
+    "Bootstrap": "https://raw.githubusercontent.com/github/explore/main/topics/bootstrap/bootstrap.png",
 };
+
+
 
 export default function Projects() {
     const [repositories, setRepositories] = useState([]);
     const [languages, setLanguages] = useState({});
 
     useEffect(() => {
-        fetch('https://api.github.com/users/thigasfella/repos')
+        const headers = {
+            Authorization: `token ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`
+        };
+        fetch('https://api.github.com/users/thigasfella/repos', { headers })
             .then(response => response.json())
             .then(data => {
                 setRepositories(data);
                 // Fetch languages for each repository
                 data.forEach(repo => {
-                    fetch(repo.languages_url)
+                    fetch(repo.languages_url, { headers })
                         .then(response => response.json())
                         .then(languagesData => {
                             setLanguages(prevLanguages => ({
@@ -35,24 +46,35 @@ export default function Projects() {
     return (
         <div className="card-projects-container">
             <div className="title-projects-container">
-            <h2 className="title-projects">PROJETOS</h2>
+            <div data-aos="zoom-out-up" data-aos-duration="700">
+                <h2 className="title-projects" id="projects">PROJETOS</h2>
             </div>
             {repositories.map(repository => {
-                if (repository.private === false) {
+                if (!repository.private && repository.topics.includes('portfolio')) {
                     return (
+                    <div data-aos="zoom-out-up" data-aos-duration="700">
                         <div className="card-projects" key={repository.id}>
                             <h3>{repository.name}</h3>
-                            <p>{repository.description}</p>
-                            <div className="languages-container">
-                                {languages[repository.id] && Object.keys(languages[repository.id]).map(lang => (
-                                        <img src={languageLogos[lang]} alt={lang} style={{width: '20px', height: '20px', marginRight: '5px'}} className="languages"/>
+                            <div className="description-container">
+                            <p className="description-project">{repository.description}</p>
+                            <div className="languages-container" >
+                                {languages[repository.id] && Object.keys(languages[repository.id]).map((lang, index) => (
+                                    <Image src={languageLogos[lang]} alt={lang} width={20} height={20} key={index} className="languages" />
                                 ))}
-                            </div>
+                                <div className="links-project-container">
+                                <a href={repository.url} target="_blank" rel="noopener noreferrer" className="links-project"><i class="bi bi-box-arrow-up-right"></i></a>
+                                <a href={repository.html_url} target="_blank" rel="noopener noreferrer" className="links-project"><i class="bi bi-github"></i></a>
+                                </div>
+                            </div>  
                         </div>
+                        </div>
+                    </div>
+                    
                     );
                 }
                 return null;
             })}
         </div>
+    </div>
     );
 }
