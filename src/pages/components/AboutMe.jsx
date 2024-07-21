43254419d41af1a0d7a59afed80176c380699987
Thiago Lopes de Souza <thiagolpssouza@gmail.com>
2024-07-21 00:52:56 -0300

    atualização aboutme

--- src/pages/components/AboutMe.jsx
+++ src/pages/components/AboutMe.jsx
@@ -6,7 +6,7 @@ export default function AboutMe(){
             <div className="text" data-aos="zoom-out-down">
                 <h2 className='title-aboutMe'>SOBRE MIM</h2>
 
-                <p className='subtitle-aboutMe'>Olá! Sou Thiago, um desenvolvedor fullstack de 19 anos, apaixonado por tecnologia. Adoro criar soluções web com React, Node.js, e mais. Recentemente, concluí cursos de JavaScript Fullstack e TypeScript. Estou sempre buscando novos desafios para crescer e aprender. Fora da programação, gosto de explorar novas tecnologias e colaborar em projetos que façam a diferença. Estou ansioso para aplicar minhas habilidades em projetos inovadores e contribuir para o sucesso deles.</p>
+                <p className='subtitle-aboutMe'>Olá, meu nome é Thiago Lopes de Souza e sou desenvolvedor web. Tenho habilidades em Next.js (React), JavaScript, TypeScript, Node.js, MongoDB, Git, Bootstrap e React. Me formei no curso Full Stack JavaScript da OneBitCode e atualmente estou cursando Análise e Desenvolvimento de Sistemas na faculdade Anhanguera. Sou dedicado e sempre busco contribuir em projetos sempre que posso.</p>
             </div>
         </div>
     )
