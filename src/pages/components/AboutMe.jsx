153593e6b4142b80b99f84517e345144436755b4
Thiago Lopes de Souza <thiagolpssouza@gmail.com>
2024-07-21 00:56:26 -0300

    atualização aboutme

--- src/pages/components/AboutMe.jsx
+++ src/pages/components/AboutMe.jsx
@@ -6,7 +6,7 @@ export default function AboutMe(){
             <div className="text" data-aos="zoom-out-down">
                 <h2 className='title-aboutMe'>SOBRE MIM</h2>
 
-                <p className='subtitle-aboutMe'>Olá, meu nome é Thiago Lopes de Souza e sou desenvolvedor web. Tenho habilidades em Next.js (React), JavaScript, TypeScript, Node.js, MongoDB, Git, Bootstrap e React. Me formei no curso Full Stack JavaScript da OneBitCode e atualmente estou cursando Análise e Desenvolvimento de Sistemas na faculdade Anhanguera. Sou dedicado e sempre busco contribuir em projetos sempre que posso.</p>
+                <p className='subtitle-aboutMe'>Olá, meu nome é Thiago Lopes de Souza e sou desenvolvedor web. Tenho habilidades em Next.js, React, JavaScript, TypeScript, Node.js, MongoDB, Git e Bootstrap. Me formei no curso Full Stack JavaScript da OneBitCode e atualmente estou cursando Análise e Desenvolvimento de Sistemas na faculdade Anhanguera. Sou dedicado e sempre busco contribuir em projetos sempre que posso.</p>
             </div>
         </div>
     )
