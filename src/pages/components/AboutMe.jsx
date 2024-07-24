fb4a5cf482b32003211ec591d6a1cbc80ada1109
Thiago Lopes de Souza <thiagolpssouza@gmail.com>
2024-07-24 12:42:51 -0300

    adicionando estilos e animações, com mudanças no favicon e responsividade

--- src/pages/components/AboutMe.jsx
+++ src/pages/components/AboutMe.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function AboutMe(){
     return(
         <div className="aboutMe-container" id="aboutMe">
-            <div className="text" data-aos="flip-right">
+            <div className="text" data-aos="zoom-out">
                 <h2 className='title-aboutMe'>SOBRE MIM</h2>
 
                 <p className='subtitle-aboutMe'>Olá, meu nome é Thiago Lopes de Souza e sou desenvolvedor web apaixonado por tecnologia e inovação. Possuo habilidades em Next.js, React, JavaScript, TypeScript, Node.js, MongoDB, Git e Bootstrap, que me permitem criar soluções web robustas e eficientes. Completei o curso Full Stack JavaScript da OneBitCode, onde desenvolvi uma sólida base em desenvolvimento web, e atualmente estou aprofundando meus conhecimentos em Análise e Desenvolvimento de Sistemas na faculdade Anhanguera. Sou um profissional dedicado, sempre buscando oportunidades para contribuir e colaborar em projetos desafiadores e enriquecedores. Estou sempre em busca de aprender e crescer, tanto em habilidades técnicas quanto na prática de trabalho em equipe.</p>
